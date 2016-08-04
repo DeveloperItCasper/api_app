07c87e1b31c0cb25afb16e4e613b1a96e16c15d6
developer <developer@itcasper.com>
2016-08-04 21:57:55 +0900

    Add model Content

---
+++ db/migrate/20160804125144_create_contents.rb
@@ -0,0 +1,11 @@
+class CreateContents < ActiveRecord::Migration
+  def change
+    create_table :contents do |t|
+      t.string :url, null: false
+      t.string :headers, array: true, default: []
+      t.string :links, array: true, default: []
+
+      t.timestamps null: false
+    end
+  end
+end
